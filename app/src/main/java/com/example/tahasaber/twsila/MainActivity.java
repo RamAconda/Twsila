9fb2ac8c3e75ac7ebae27830f9875e3ba1219d02
Ramadan_Anaconda <rmadanfci20130197@gmail.com>
2017-06-12 04:53:43 +0200

    application is working, but you have to enable the location before runing the app

--- app/src/main/java/com/example/tahasaber/twsila/MainActivity.java
+++ app/src/main/java/com/example/tahasaber/twsila/MainActivity.java
@@ -20,6 +20,8 @@ import android.view.View;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks
         , GoogleApiClient.OnConnectionFailedListener {
@@ -33,12 +35,25 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
 
+    //firebase auth variable
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
     private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing firebase auth and user.
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        /*if(mUser == null){
+            Intent signInIntent = new Intent(this , SignUpActivity.class);
+            startActivity(signInIntent);
+        }*/
+
         defineGoogleApiClient();
         getLocation();
         //begin the posts fragment logic(code)
--- app/src/main/java/com/example/tahasaber/twsila/PostsViewFragment.java
+++ app/src/main/java/com/example/tahasaber/twsila/PostsViewFragment.java
@@ -2,9 +2,12 @@ package com.example.tahasaber.twsila;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -14,8 +17,10 @@ import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +30,17 @@ import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
 import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationServices.*;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -135,24 +151,56 @@ public class PostsViewFragment extends Fragment {
         geofireToSearchPosts = new GeoFire(geofireReference);
 
         //set the query on the current location and around the user with 1 kilo meter.
-        updateLocation();
+        InitializeLocation();
         //we update the userLocation when the listener calls the onChangeLocation
         //so when we here update the location a single time then the userLocation will
         //be updated so I can use it in the next line of code.
         //locationManager.requestSingleUpdate("gps", locationListener, null);
-        geoQueryToSearchPosts = geofireToSearchPosts.queryAtLocation(
-                /*userLocation*/getLastKnownLocation()/*new GeoLocation(29.9061584,31.2710861)*/, 1);
+        GeoLocation geoLocation = getLastKnownLocation();
+        geoQueryToSearchPosts = geofireToSearchPosts.queryAtLocation(geoLocation, 1);
 
         //creating the listener and adding it to the geoQueryToSearchPosts.
         attachTheGeoQueryListener();
 
     }
 
+
+    //this function is all about making alert to enable the gps if
+    //it's disabled,
+    /*private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void enableGPS(){
+        Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+        intent.putExtra("enabled" , true);
+        getActivity().sendBroadcast(intent);
+    }*/
+
     private GeoLocation getLastKnownLocation() {
-        GeoLocation geoLocation = null;
+        /*if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            buildAlertMessageNoGps();
+        }*/
+
         List<String> providers = locationManager.getProviders(true);
         Location location=null;
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity()
+                , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{
                     Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -169,8 +217,10 @@ public class PostsViewFragment extends Fragment {
                 location = l;
             }
         }
-        geoLocation = new GeoLocation(location.getLatitude() , location.getLongitude());
-        return geoLocation;
+        return new GeoLocation(location.getLatitude() , location.getLongitude());
+
+        /*LocationUtils utils = new LocationUtils();
+        return utils.getLastLocation(getActivity());*/
     }
 
     //function to initialize the geofire query listener
@@ -242,55 +292,63 @@ public class PostsViewFragment extends Fragment {
         }
     }
 
-    private GeoLocation updateLocation() {
+    private void /*GeoLocation*/ InitializeLocation() {
         //first time to get location, which means there is no listener on the location
-        if (locationManager == null) {
-            //define the location manager using the parent activity.
-            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            // the location listener logic
-            locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    if (geoQueryToSearchPosts != null)
-                        geoQueryToSearchPosts.setCenter(new GeoLocation(location.getLatitude(),
-                                location.getLongitude()));
-                    userLocation = new GeoLocation(location.getLatitude(),
-                            location.getLongitude());
+        //define the location manager using the parent activity.
+        //locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        // the location listener logic
+
+        enableLocation();
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                GeoLocation geoLocation;
+                if(location == null){
+                    geoLocation = getLastKnownLocation();
                 }
-
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
-
+                else{
+                    geoLocation = new GeoLocation(location.getLatitude(),location.getLongitude());
                 }
+                if (geoQueryToSearchPosts != null)
+                    geoQueryToSearchPosts.setCenter(geoLocation);
+                //userLocation = geoLocation;
+            }
 
-                @Override
-                public void onProviderEnabled(String s) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String s) {
-                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(intent);
-                }
-
-            };
-
-
-            if (ActivityCompat.checkSelfPermission(getActivity(),
-                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(getActivity(),
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                requestPermissions(new String[]{
-                        android.Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.INTERNET}, PERMISSION_RESOLVER_CODE);
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
             }
-            locationManager.requestLocationUpdates("gps", /*10 minutes*/10 * 60 * 1000, 0, locationListener);
-        }
-        return userLocation;
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                /*Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);*/
+                enableLocation();
+            }
+
+        };
+
+
+        /*if (ActivityCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET}, PERMISSION_RESOLVER_CODE);
+
+        }*/
+        locationManager.requestLocationUpdates("gps", /*10 minutes*/10 * 60 * 1000, 0, locationListener);
+        //return userLocation;
     }
 
 
@@ -315,12 +373,12 @@ public class PostsViewFragment extends Fragment {
         return rootView;
     }
 
-    @Override
+    /*@Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_RESOLVER_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates("gps", /*10 minutes*/10 * 60 * 1000, 0, locationListener);
+                    locationManager.requestLocationUpdates("gps", *//*10 minutes*//*10 * 60 * 1000, 0, locationListener);
                     break;
                 }
             case REQUEST_PERMISSION_CODE:
@@ -330,5 +388,50 @@ public class PostsViewFragment extends Fragment {
                 }
 
         }
+    }*/
+
+    private void enableLocation(){
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            public static final String TAG = "hello";
+            public static final int REQUEST_CHECK_SETTINGS = 0;
+
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.i(TAG, "All location settings are satisfied.");
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result
+                            // in onActivityResult().
+                            status.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.i(TAG, "PendingIntent unable to execute request.");
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                        break;
+                }
+            }
+        });
     }
+
 }
